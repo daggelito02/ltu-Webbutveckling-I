0060d665b36765f937f568c79009d718a8f3aae1
daggelito02 <daggelito02@gmail.com>
2024-12-09 20:03:28 +0100

    compact menu yes

--- Uppgift_4/javascript/cookie_consent.js
+++ Uppgift_4/javascript/cookie_consent.js
@@ -33,3 +33,11 @@ document.addEventListener('DOMContentLoaded', function() {
         return null;
     }
 });
+
+const dropdownToggle = document.getElementById('menu-toggle');
+const dropdownMenu = document.querySelector('.menu-dropdown');
+
+dropdownToggle.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('open'); // Växlar öppet/stängt läge
+  dropdownToggle.classList.toggle('selected'); // Växlar öppet/stängt läge
+});
